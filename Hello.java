6da603e6f610a21a3cf821acd40b377144b0cf14
Mithun <mithuncm@gmail.com>
2023-02-14 21:28:59 +0530

    ENTRYPOINT, Exec way, Making a Docker image with a Dockerfile

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Docker.!");
+        if (args != null && args.length > 0) {
+            System.out.println("Hello Docker.!" + args[0] + "!");
+        } else {
+            System.out.println("Hello World.!");
+        }
     }
 }
